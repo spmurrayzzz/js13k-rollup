48ad51a9b3940c762d99f4a7375ce3a9c8fb72ea
davidtranhq <davidtranhq+github@gmail.com>
2020-08-16 21:01:04 -0400

    Fixed Gulp4 requiring watch()'s argument to be a function

--- tasks/watch.js
+++ tasks/watch.js
@@ -2,12 +2,15 @@
 
 const gulp = require('gulp');
 const livereload  = require('gulp-livereload');
+const distTasks = require('./dist.js');
 
 function watch() {
   livereload.listen();
   return gulp.watch(
     [ 'src/js/**/*.js', 'src/css/**/*.css', 'src/index.hbs' ],
-    [ 'build', 'css', 'template', 'zip', 'report' ]
+    // Gulp4 requires a function to be passed to watch()
+    // a call to series() with one task returns a function
+    gulp.series(distTasks.dist)
   );
 }
 
